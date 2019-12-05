61cf2595c21137288655ed38604280a89f949a01
anushakartala <57739547+anushakartala@users.noreply.github.com>
2019-12-04 23:50:26 -0600

    Create Config.java

---
+++ Config.java
@@ -0,0 +1,11 @@
+// This class is for all our configuration data. By putting it all in one place, we can easily make changes to the program without having to hunt down where in the code a constant is defined.
+
+public class Config {
+  public final static String DATAPATH = "/Users/anushakartala/eclipse-workspace/FinalProject/src/"; // If you don't know what the static keyword does, you better go look it up now.
+  public static String APPLICATIONNAME = "Hexagon Cross For Less";
+  public static String PrimeFileName = "primes.txt";
+  public static String CrossFileName = "crosses.txt";
+  public static String Title = "Hexagon Cross for Less";
+  // Prime file: One prime per line.
+  // Cross file: Two primes per line, separated by a comma.
+} 
